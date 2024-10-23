63742f723b003384086beec6f616a9e00e91a3f5
Krow <katie.scantland@stu.glasgow.kyschools.us>
2024-10-23 08:35:46 -0500

    Initial Commit

---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Krow on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
